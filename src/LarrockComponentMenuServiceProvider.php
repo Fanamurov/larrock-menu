5daf1f8e4b896841673cd78acc07838bc19b7c99
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-27 17:51:11 +1000

    Fix

--- src/LarrockComponentMenuServiceProvider.php
+++ src/LarrockComponentMenuServiceProvider.php
@@ -37,7 +37,7 @@ class LarrockComponentMenuServiceProvider extends ServiceProvider
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/database/migrations/0000_00_00_000000_create_blocks_table.php' => database_path('migrations/'.$timestamp.'_create_blocks_table.php')
+                __DIR__.'/database/migrations/0000_00_00_000000_create_menu_table.php' => database_path('migrations/'.$timestamp.'_create_menu_table.php')
             ], 'migrations');
         }
     }
