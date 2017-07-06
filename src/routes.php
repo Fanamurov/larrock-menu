4dd015de6f0a2c818b11d5d2c72634a1d9765df1
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:34:50 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,6 @@
 
 use Larrock\ComponentMenu\AdminMenuController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('menu', AdminMenuController::class);
 });
