324dd73b9a5ba48b3eef22e652ed4ec5abe6f724
Alexandr Fanamurov <fanamura@gmail.com>
2017-08-14 17:14:09 +1000

    fix template

--- src/views/front/modules/menu/top.blade.php
+++ src/views/front/modules/menu/top.blade.php
@@ -1,4 +1,4 @@
-<nav class="uk-navbar uk-hidden-small" id="top_menu_block">
+<nav class="uk-navbar uk-hidden-small" id="{{ $menu->first()->type }}_menu_block">
     <ul class="uk-navbar-nav">
         @foreach($menu as $data_item)
             <li @if(
