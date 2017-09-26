cb905e977c975f10188556fa9c569c52f9b585ca
Alexandr Fanamurov <fanamura@gmail.com>
2017-09-26 17:37:41 +1000

    add mobile button

--- src/views/front/modules/menu/top.blade.php
+++ src/views/front/modules/menu/top.blade.php
@@ -1,3 +1,5 @@
+<button class="uk-button uk-button-large uk-button-primary uk-width-1-1 uk-hidden-medium uk-hidden-large"
+        data-uk-toggle="{target:'#{{ $menu->first()->type }}_menu_block', cls:'uk-hidden-small'}" onclick="$(this).hide()">Меню</button>
 <nav class="uk-navbar uk-hidden-small" id="{{ $menu->first()->type }}_menu_block">
     <ul class="uk-navbar-nav">
         @foreach($menu as $data_item)
