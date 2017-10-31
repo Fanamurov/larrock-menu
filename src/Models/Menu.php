ab43b4c55df5c9f0022b9bbf2d28c7f866b0847b
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 17:58:55 +1000

    add scopeActive

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -47,9 +47,9 @@ class Menu extends Model
         return $this->hasMany(LarrockMenu::getModelName(), 'parent', 'id')->orderBy('position', 'DESC');
     }
 
-    public function get_childActive()
+    public function scopeActive($query)
     {
-        return $this->hasMany(LarrockMenu::getModelName(), 'parent', 'id')->whereActive(1)->orderBy('position', 'DESC');
+        return $query->where('active', 1);
     }
 
     public function getParentTreeAttribute()
@@ -74,10 +74,5 @@ class Menu extends Model
     public function get_parent()
     {
         return $this->hasOne(LarrockMenu::getModelName(), 'id', 'parent');
-    }
-
-    public function get_parentActive()
-    {
-        return $this->hasOne(LarrockMenu::getModelName(), 'id', 'parent')->whereActive(1);
     }
 }
